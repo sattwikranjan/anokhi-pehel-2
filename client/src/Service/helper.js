8711e93c6979f437bb1af1123c95f2b1bd75780a
VIVEK KUMAR <kumarvivek7282827749@gmail.com>
2024-04-15 22:47:50 +0530

    fixed bug in backend

--- client/src/Service/helper.js
+++ client/src/Service/helper.js
@@ -1,4 +1,5 @@
 // export const BASE_URL = "http://localhost:8080/api/v1/user";
 // export const SERVER_URL = "http://localhost:8080";
+
 export const BASE_URL = "https://anokhi-pehel.azurewebsites.net/api/v1/user";
 export const SERVER_URL = "https://anokhi-pehel.azurewebsites.net";
