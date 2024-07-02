e1da584eb0626f28b2d309e58c5864c3cd53cd36
Sattwik Ranjan <sattwikranjan@gmail.com>
2024-07-02 11:06:47 +0530

    topic covered pagination

--- client/src/pages/Dashboard/FindTopic.jsx
+++ client/src/pages/Dashboard/FindTopic.jsx
@@ -2,13 +2,14 @@ import DashboardLayout from "../../components/Dashboard/DashboardLayout";
 import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import { classes, subjects } from "../../constants/Dashboard";
+import { classes, months, subjects } from "../../constants/Dashboard";
 import { BASE_URL } from "../../../src/Service/helper";
 import { useSelector } from "react-redux";
+import Pagination from "../../components/Dashboard/Pagination";
 
 const FindTopic = () => {
   const navigate = useNavigate();
-
+  const initialTopics = 10;
   const { user } = useSelector((state) => state.user);
   const [errors, setErrors] = useState({});
   const [credentials, setCredentials] = useState({
@@ -16,13 +17,19 @@ const FindTopic = () => {
     subject: "",
   });
   const [topics, setTopics] = useState([]);
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const [topicsPerPage, setTopicsPerPage] = useState(initialTopics);
   const onChange = (e) => {
     const { name, value } = e.target;
     setCredentials({ ...credentials, [name]: value });
     if (name === "classId") {
       fetchTopicCovered(value, credentials.subject);
     }
+    if (name === "subject") {
+      fetchTopicCovered(credentials.classId, value);
+    }
+    setTopicsPerPage(initialTopics);
+    setCurrentPage(1);
   };
 
   const fetchTopicCovered = async (classId, subject) => {
@@ -35,11 +42,13 @@ const FindTopic = () => {
       });
       // console.log(response.data);
       const { topics: receivedTopics } = response.data;
-
-      if (Array.isArray(receivedTopics)) {
-        setTopics(receivedTopics);
+      const sortedTopics = receivedTopics.sort(
+        (a, b) => new Date(b.date) - new Date(a.date)
+      ); // Sorting by date
+      if (Array.isArray(sortedTopics)) {
+        setTopics(sortedTopics);
         const topicsWithMentorNames = await Promise.all(
-          receivedTopics.map(async (topic) => {
+          sortedTopics.map(async (topic) => {
             try {
               const mentorResponse = await axios.get(
                 `${BASE_URL}/getMentorByUserId?mentorId=${topic.mentorId}`
@@ -69,6 +78,36 @@ const FindTopic = () => {
       setTopics([]); // Reset topics on error
     }
   };
+
+  // Calculate the indices for the current page
+  const indexOfLastTopic = currentPage * topicsPerPage;
+  const indexOfFirstTopic = indexOfLastTopic - topicsPerPage;
+  const currentTopics = topics.slice(indexOfFirstTopic, indexOfLastTopic);
+
+  // Function to handle page change
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+  const handlePreviousPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+  };
+
+  const handleNextPage = () => {
+    if (currentPage < totalPages) {
+      setCurrentPage(currentPage + 1);
+    }
+  };
+  // Calculate total pages
+  const totalPages = Math.ceil(topics.length / topicsPerPage);
+
+  const handleUsersPerPageChange = (e) => {
+    setTopicsPerPage(Number(e.target.value));
+    setCurrentPage(1);
+  };
+
   return (
     <DashboardLayout>
       <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -147,14 +186,20 @@ const FindTopic = () => {
                     </tr>
                   </thead>
                   <tbody>
-                    {topics.map((topic, index) => (
+                    {currentTopics.map((topic, index) => (
                       <tr
                         key={topic._id}
                         className={index % 2 === 0 ? "bg-gray-100" : "bg-white"}
                       >
                         <td className="py-2 px-4">{index + 1}</td>
                         <td className="py-2 px-4">
-                          {new Date(topic.date).toLocaleDateString()}
+                          {topic.date.split("-")[2].split("T")[0]}{" "}
+                          {
+                            months.find(
+                              (m) => m.value === topic.date.split("-")[1]
+                            )?.label
+                          }{" "}
+                          {topic.date.split("-")[0]}
                         </td>
                         <td className="py-2 px-4">{topic.topic}</td>
                         <td className="py-2 px-4">{topic.mentorName}</td>
@@ -166,6 +211,17 @@ const FindTopic = () => {
             </div>
           </div>
         </form>
+        <Pagination
+          currentPage={currentPage}
+          totalPages={totalPages}
+          onPageChange={handlePageChange}
+          onPreviousPage={handlePreviousPage}
+          onNextPage={handleNextPage}
+          initialUsers={initialTopics}
+          usersPerPage={topicsPerPage}
+          handleUsersPerPageChange={handleUsersPerPageChange}
+          totalUsers={topics.length}
+        />
       </div>
     </DashboardLayout>
   );
